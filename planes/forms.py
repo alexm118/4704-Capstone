d491608f9280426733cbc8753cc76f33a61b18b2
Alex Martin <alexander.martin@excella.com>
2016-11-07 15:50:09 -0500

    Added the pager, but need to fix some issues with it

--- planes/forms.py
+++ planes/forms.py
@@ -4,6 +4,7 @@ from planes.models import Manufacturer, Plane
 
 class PlaneForm(forms.ModelForm):
     manufacturer = forms.ModelChoiceField(queryset=Manufacturer.objects.all())
+    page = forms.IntegerField(required=True, widget=forms.HiddenInput())
 
     class Meta:
         model = Plane
--- planes/views.py
+++ planes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http.response import JsonResponse
 from planes.models import AirbusPlane, Plane, Manufacturer, GulfstreamPlane, BoeingPlane
-from planes.serializers import AirbusPlaneSerializer
+from planes.serializers import AirbusPlaneSerializer, BoeingPlaneSerializer
 from django.http import JsonResponse
 from planes.forms import PlaneForm
+from planes.utils import Pager
 
 
 def display_airbus_plane(request, id):
@@ -23,7 +26,11 @@ def display_boeing_plane(request, id):
 def list_planes(request):
     planes = Plane.objects.all()
     form = PlaneForm()
-    return render(request, "planes/plane_list.html", context={'planes': planes, 'form': form})
+    pager = Pager()
+    pager.page_size = 10
+    pager.data_set = planes
+    pager.get_paged_items()
+    return render(request, "planes/plane_list.html", context={'planes': pager.paged_items, 'form': form, 'pager': pager})
 
 
 def airbus_rest_plane(request, id):
@@ -31,10 +38,12 @@ def airbus_rest_plane(request, id):
     serializer = AirbusPlaneSerializer(plane)
     return JsonResponse(serializer.data)
 
+
 def boeing_rest_plane(request, id):
     plane = BoeingPlane.objects.get(id=id)
     serializer = BoeingPlaneSerializer(plane)
     return JsonResponse(serializer.data)
+
 
 def filter_plane_list(request, id):
     if request.method == 'GET':
@@ -43,7 +52,19 @@ def filter_plane_list(request, id):
             planes = Plane.objects.filter(manufacturer=manufacturer)
         else:
             planes = Plane.objects.all()
-        return render(request, 'planes/panels/plane_table.html', context={'planes': planes})
+        pager = Pager()
+        pager.page_size = 10
+        pager.data_set = planes
+        pager.get_paged_items()
+        context = {
+            'planes': pager.paged_items,
+            'pager': pager
+        }
+        return_data = {
+            'planes': render_to_string('planes/panels/plane_table.html', context=context),
+            'pager': render_to_string('planes/panels/pager.html', context=context)
+        }
+        return JsonResponse(return_data)
     else:
         print('Invalid access')
 
