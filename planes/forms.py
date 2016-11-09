b05cc15b29da135931dd7f6dce8f521b0cbd5c91
Alex Martin <alexander.martin@excella.com>
2016-11-09 15:37:37 -0500

    Changed some themes, pagination works, filtering doesn't.

--- planes/forms.py
+++ planes/forms.py
@@ -3,7 +3,7 @@ from planes.models import Manufacturer, Plane
 
 
 class PlaneForm(forms.ModelForm):
-    manufacturer = forms.ModelChoiceField(queryset=Manufacturer.objects.all())
+    manufacturer = forms.ModelChoiceField(required=False, queryset=Manufacturer.objects.all())
     page = forms.IntegerField(required=True, widget=forms.HiddenInput())
 
     class Meta:
--- planes/views.py
+++ planes/views.py
@@ -24,13 +24,39 @@ def display_boeing_plane(request, id):
 
 
 def list_planes(request):
-    planes = Plane.objects.all()
-    form = PlaneForm()
-    pager = Pager()
-    pager.page_size = 10
-    pager.data_set = planes
-    pager.get_paged_items()
-    return render(request, "planes/plane_list.html", context={'planes': pager.paged_items, 'form': form, 'pager': pager})
+    if request.method == "POST":
+        form = PlaneForm(request.POST)
+        if form.is_valid():
+            page = form.cleaned_data['page']
+            manufacturer = form.cleaned_data['manufacturer']
+            if manufacturer:
+                planes = Plane.objects.filter(manufacturer=manufacturer).order_by('manufacturer', 'model')
+            else:
+                planes = Plane.objects.all().order_by('manufacturer', 'model')
+            pager = Pager()
+            pager.page_size = 25
+            pager.data_set = planes
+            pager.get_paged_items(page)
+            context = {
+                'planes': pager.paged_items,
+                'pager': pager
+            }
+            return_data = {
+                'planes': render_to_string('planes/panels/plane_table.html', context=context),
+                'pager': render_to_string('planes/panels/pager.html', context=context)
+            }
+            return JsonResponse(return_data)
+        else:
+            print(form.errors)
+
+    else:
+        planes = Plane.objects.all().order_by('manufacturer', 'model')
+        form = PlaneForm()
+        pager = Pager()
+        pager.page_size = 25
+        pager.data_set = planes
+        pager.get_paged_items()
+        return render(request, "planes/plane_list.html", context={'planes': pager.paged_items, 'form': form, 'pager': pager})
 
 
 def airbus_rest_plane(request, id):
@@ -47,11 +73,8 @@ def boeing_rest_plane(request, id):
 
 def filter_plane_list(request, id):
     if request.method == 'GET':
-        if int(id) != 10000:
-            manufacturer = get_object_or_404(Manufacturer, id=id)
-            planes = Plane.objects.filter(manufacturer=manufacturer)
-        else:
-            planes = Plane.objects.all()
+        manufacturer = Manufacturer.objects.filter(id=id)
+        planes = Plane.objects.filter(manufacturer=manufacturer).order_by('model')
         pager = Pager()
         pager.page_size = 10
         pager.data_set = planes
